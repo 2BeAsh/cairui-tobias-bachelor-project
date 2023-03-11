0e9ef3652ed9b8d2b8e4c041fca78828eb1b7b8c
2BeAsh <thefatfishmclol@gmail.com>
2023-03-11 12:03:12 +0100

    Multiple modes
    Updated versions of fluid velocities which has one less loop.

--- Fluid/fluid_module.py
+++ Fluid/fluid_module.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.special import lpmn
 
 
 def field_polar_lab(r, theta, phi, B_11, B_tilde_11, B_01):
@@ -34,47 +35,110 @@ def field_cartesian_squirmer(r, theta, phi, a, B_11, B_tilde_11, B_01):
     u_y = u_r * np.sin(theta) * np.sin(phi) + u_theta * np.cos(theta) * np.sin(phi) + u_phi * np.cos(phi)
     u_x = u_r * np.sin(theta) * np.cos(phi) + u_theta * np.cos(theta) * np.cos(phi) - u_phi * np.sin(phi)
     
-    #u_z += B_01 * 4 / (3 * a ** 3)
-    #u_y += -B_tilde_11 * 4 / (3 * a ** 3)
-    #u_x += -B_11 * 4 / (3 * a ** 3)
+    u_z += B_01 * 4 / (3 * a ** 3)
+    u_y += -B_tilde_11 * 4 / (3 * a ** 3)
+    u_x += -B_11 * 4 / (3 * a ** 3)
     return u_x, u_y, u_z
+
+
+def field_polar_lab_updated(N, r, theta, a, B, B_tilde, C, C_tilde):
+    """Calculate the field in polar coordinates
+
+    Args:
+        N (int larger than 1): Max possible mode
+        r (float): Distance between target and agent (prey and squirmer)
+        theta (float): Angle between vertical axis z and target
+        a (float): Squirmer radius
+        B ((N+1, N+1)-array): Modes
+        B_tilde ((N+1, N+1)-array): Modes
+        C ((N+1, N+1)-array)): Modes
+        C_tilde ((N+1, N+1)-array): Modes
+
+    Returns:
+        u_r (float): 
+            Velocity in the radial direction
+        u_theta (float):
+            Angular velocity        
+    """
+    phi = np.pi / 2
+    LP, LP_deriv = lpmn(N, N, np.cos(theta))
+    r_first_term = 4 / (3 * r ** 3) * (B[1, 1] * np.sin(theta) * np.cos(phi) 
+                                       + B_tilde[1, 1] * np.sin(theta) * np.sin(phi) 
+                                       - B[0, 1] * np.cos(theta))
+    theta_first_term = - 2 / (3 * r ** 3) * (B[1, 1] * np.cos(theta) * np.cos(phi)
+                                             + B_tilde[1, 1] * np.cos(theta) * np.sin(phi)
+                                             + B[0, 1] * np.sin(theta))
+    u_r = r_first_term
+    u_theta = theta_first_term 
+    for n in np.arange(2, N+1):  # Sum starts at n=2 and ends at N
+        m_arr = np.arange(n+1)  # Since endpoints are not included, all must +1
+        #mask = (slice(n+1), slice(n))
+        LP_arr = LP[:n+1, n]
+        LP_deriv_arr = LP_deriv[:n+1, n]
+        B_arr = B[:n+1, n]
+        B_tilde_arr = B[:n+1, n]
+        C_arr = C[:n+1, n]
+        C_tilde_arr = C_tilde[:n+1, n]
+        # Array with velocities for each m can be summed to give the total value of the inner sum.
+        u_r_arr = ((n + 1) * LP_arr / r ** (n + 2)
+                   * ((r / a) ** 2 - 1)
+                   * (B_arr * np.cos(m_arr * phi) + B_tilde_arr * np.sin(m_arr * phi)) 
+        )
+        
+        u_theta_arr = (np.sin(theta) * LP_deriv_arr
+                   * ((n - 2) / (n * a ** 2 * r ** n) - 1 / r ** (n + 2))
+                   * (B_arr * np.cos(phi) + B_tilde_arr * np.sin(m_arr * phi))
+                   + m_arr * LP_arr / (r ** (n + 1) * np.sin(theta)) 
+                   * (C_tilde_arr * np.cos(m_arr * phi) - C_arr * np.sin(m_arr * phi))
+        )
+        u_r += np.sum(u_r_arr)
+        u_theta += np.sum(u_theta_arr)
+     
+    return u_r, u_theta
     
+def field_cartesian_squirmer(N, r, theta, a, B, B_tilde, C, C_tilde):
+    """Convert polar velocities to cartesian
+    
+    Args:
+        N (int larger than 1): Max possible mode
+        r (float): Distance between target and agent (prey and squirmer)
+        theta (float): Angle between vertical axis z and target
+        a (float): Squirmer radius
+        B ((N+1, N+1)-array): Modes
+        B_tilde ((N+1, N+1)-array): Modes
+        C ((N+1, N+1)-array)): Modes
+        C_tilde ((N+1, N+1)-array): Modes
+
+    Returns:
+        u_r (float): 
+            Velocity in the radial direction
+        u_theta (float):
+            Angular velocity        
+    """
+    phi = np.pi / 2
+    u_phi = 0
+    u_r, u_theta, u_phi = field_polar_lab(N, r, theta, a, B, B_tilde, C, C_tilde)
+    
+    u_z = np.cos(theta) * u_r - np.sin(theta) * u_theta
+    u_y = u_r * np.sin(theta) * np.sin(phi) + u_theta * np.cos(theta) * np.sin(phi) + u_phi * np.cos(phi)
+    
+    return u_y, u_z
+
+
+
 
 if __name__ == "__main__":
-    from scipy.special import lpmv, lpmn
-
-
-    def P_legendre(n, m):
-        pass
-
-    def field_polar_no_loop(distance, squirmer_radius, N, B, B_tilde):
-        """
-        distance (float): Distance between agent and target
-        squirmer_radius (float): Radius of the squirmer agent
-        N (int): Upper limit of n-sum
-        B 
-        """
-        n_vals = np.arange(1, N+1)
-        phi = np.pi / 2
-        u_r = 0
-        u_theta = 0
-        for m in range(N):
-            m_vals = np.arange(m)
-            indices = tuple(zip(m_vals, n_vals-1))  # n values and indices are shifted by 1
-            u_r += np.sum(
-                (n_vals + 1) * P_legendre(n_vals, m_vals) / np.power(distance, n_vals+2)
-                * ((distance / squirmer_radius) ** 2 - 1)
-                * (B[indices] * np.cos(m_vals * phi) + B_tilde[indices] * np.sin(m_vals * phi))
-            )
-            u_theta += (
-                
-            )
-
-
-    xx = np.cos(np.linspace(0, 2 * np.pi, 10))
-    m = 1
-    n = 2
+    N = 2
+    r = 3
+    a = 1
+    B = np.random.uniform(size=(N+1, N+1))
+    B_tilde = B / 2
+    C = B / 3
+    C_tilde = B / 4
     theta = np.pi/2
-    val, diffval = lpmn(m, n, np.cos(theta))
-    #print(diffval)
-    #print(legendre_poly_prime(n, m, theta))
+    #print(np.arange(10))
+    #print(np.arange(10)[:2])
+    vals, diff = lpmn(N, N, np.cos(theta))
+    #print(vals[:N, N])
+    u_r, u_theta = field_polar_lab_updated(N, r, theta, a, B, B_tilde, C, C_tilde)
+    print(u_r, u_theta)
