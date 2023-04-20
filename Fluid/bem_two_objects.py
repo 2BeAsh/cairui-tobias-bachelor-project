45e442bc23a54527cc4942af3a5a1d6967012f23
2BeAsh <thefatfishmclol@gmail.com>
2023-04-20 13:47:12 +0200

    Update bem_two_objects.py
    two objects now fully work

--- Fluid/bem_two_objects.py
+++ Fluid/bem_two_objects.py
@@ -15,16 +15,18 @@ def difference_vectors(x_1, x_2=None):
     return dx, dy, dz
 
 
-def torque_condition(x_tupple):
+def torque_condition(x_stacked):
     """The torque sums to 0 condition for the ROWS. To get the column, transpose the result
 
     Args:
-        x_tupple (Tupple of 1darrays): Tuple of the (x, y, z) coordinates
+        x_stacked (N, 3)-array: array with the (x, y, z) coordinates
 
     Returns:
         (3, 3N)-array: Torque condition along rows.
     """
-    x, y, z = x_tupple
+    x = x_stacked[:, 0]
+    y = x_stacked[:, 1]
+    z = x_stacked[:, 2]
     N = len(x)
     torque_arr = np.zeros((3, 3*N))
     torque_arr[0, N: 2*N] = -z
@@ -36,7 +38,7 @@ def torque_condition(x_tupple):
     return torque_arr
 
 
-def oseen_tensor_surface_two_objects(x_1, x_2, center_vector, dA, regularization_offset, viscosity):
+def oseen_tensor_surface_two_objects(x_1, x_2, x1_center, x2_center, dA, regularization_offset, viscosity):
     """Find Oseen tensor for all points on the surface of the squirmer, another object and their interaction with each other
 
     Args:
@@ -48,41 +50,39 @@ def oseen_tensor_surface_two_objects(x_1, x_2, center_vector, dA, regularization
         viscosity (float): Viscosity of the fluid.
 
     Returns:
-        (6N, 6N)-array: Oseen tensor S, N is number of points i.e. len(x)
+        (6N1+12, 6N2+12)-array: Oseen tensor S, N1=len(x_1), N2=len(x_2)
     """
     oseen_factor = dA / (8 * np.pi * viscosity)
     eps = regularization_offset
-    N1 = np.shape(x_1)[0]  # Object 1 number of points
-    N2 = np.shape(x_2)[0]
+    N1 = len(x_1)
+    N2 = len(x_2)
 
     # Difference vectors, first object 1 to 1, then 2 to 2 and then 1 to 2
-    #dx11, dy11, dz11 = difference_vectors(x_1)
-    #dx22, dy22, dz22 = difference_vectors(x_2)  # Internally doesn't need to change reference
-    dx12, dy12, dz12 = difference_vectors(x_1, x_2 + center_vector)  # Change reference to be object 1's.
+    dx12, dy12, dz12 = difference_vectors(x_1 + x1_center, x_2 + x2_center)
+    r12 = np.sqrt(dx12 ** 2 + dy12 ** 2 + dz12 ** 2)
+    r12_eps_cube = np.sqrt(r12 ** 2 + eps ** 2) ** 3
     
-    # -- Construct S matrices --  - Skulle man bruge Sparse matricer?
+    # -- Construct S matrices --  Skulle man bruge Sparse matricer?
+    S = np.zeros((3*(N1+N2)+12, 3*(N1+N2)+12))
+    
     # Consists of four submatrices. Each are symmetrical.
     # Upper left (3*N1. 3*N1) is Object 1 to itself
     # Upper right (3*N1, 3*N2) is cross terms
     # Lower left (3*N2, 3*N1) is cross terms - Transposed of upper right
     # Lower right (3*N2, 3*N2) is Object 2 to itself
     
-    S = np.zeros((3*(N1+N2)+12, 3*(N1+N2)+12))
     # Upper left and lower right can be found using previously defined functions
-
-    S[: 3*N1, : 3*N1] = bem.oseen_tensor(eps, dA, viscosity, x_1)[:-6, :-6]  # Cut off boundary condition
+    S[: 3*N1, : 3*N1] = bem.oseen_tensor(eps, dA, viscosity, x_1)[:-6, :-6]  # Cut off last 6 rows and columns from force=0=torque condition
     S[3*N1: 3*(N1+N2), 3*N1: 3*(N1+N2)] = bem.oseen_tensor(eps, dA, viscosity, x_2)[:-6, :-6]
     
     # Upper right and lower left
     S_12 = np.zeros((3*N1, 3*N2))
-    r12 = np.sqrt(dx12**2 + dy12**2 + dz12**2)
-    r12_eps_cube = np.sqrt(r12 ** 2 + eps ** 2) ** 3
 
     # Central part: S11 S22 S33
-    S_12[: N1, : N2] = (r12 ** 2 + 2 * eps ** 2 + dx12 ** 2) / r12_eps_cube
-    S_12[N1: 2*N1, N2: 2*N2] = (r12 ** 2 + 2 * eps ** 2 + dy12 ** 2) / r12_eps_cube
-    S_12[2*N1: 3*N1, 2*N2: 3*N2] = (r12 ** 2 + 2 * eps ** 2 + dz12 ** 2) / r12_eps_cube
-    
+    d11 = [dx12, dy12, dz12]
+    for i in range(3):
+        S_12[i*N1 : (i+1)*N1, i*N2 : (i+1)*N2] = (r12 ** 2 + 2 * eps ** 2 + d11[i] ** 2) / r12_eps_cube
+        
     # Right part: S12 S13 S23
     S_12[: N1, N2: 2*N2] = dx12 * dy12 / r12_eps_cube
     S_12[: N1, 2*N2: 3*N2] = dx12 * dz12 / r12_eps_cube
@@ -99,7 +99,7 @@ def oseen_tensor_surface_two_objects(x_1, x_2, center_vector, dA, regularization
     S[: 3*N1, 3*N1: 3*(N1+N2)] = S_12  # Upper right
     S[3*N1: 3*(N1+N2), : 3*N1] = S_12.T  # Lower left
     
-    # Conditions
+    # -- Conditions --
     # Force
     # First define the one-matricies for both objects, then update S according to them
     force_obj1 = np.zeros((3, 3*N1))
@@ -122,65 +122,34 @@ def oseen_tensor_surface_two_objects(x_1, x_2, center_vector, dA, regularization
     S[-3: , 3*N1: 3*(N1+N2)] = torque_obj2
     S[3*N1: 3*(N1+N2), -3:] = torque_obj2.T
     
-
-def oseen_tensor_point_two_objects(source_points_1, source_points_2, evaluation_points, dA_1, dA_2, regularization_offset, viscosity):
-    oseen_factor = dA_1 / (8 * np.pi * viscosity)
-    oseen_factor = dA_2 / (8 * np.pi * viscosity)
-    eps = regularization_offset
-    N = np.shape(source_points_1)[0]  # Along columns
-    M = np.shape(evaluation_points)[0]  # Along rows
-    print(N, M)
-    # Difference vectors, first object 1 to evaluation, then object 2 to evaluation
-    dx1, dy1, dz1, r1, r1_eps_cube = difference_vectors(eps, source_points_1, evaluation_points)
-    dx2, dy2, dz2, r2, r2_eps_cube = difference_vectors(eps, source_points_2, evaluation_points)
-    dx = [dx1, dx2]
-    dy = [dy1, dy2]
-    dz = [dz1, dz2]
-    r = [r1, r2]
-    r_eps_cube = [r1_eps_cube, r2_eps_cube]
-    oseen_factor = [oseen_factor, oseen_factor]
+    return S
     
-    # -- Construct S matrices --  - Skulle man bruge Sparse matricer?
-    # Loop through both objects. 
-    S = np.zeros((3*M+12, 6*N+12))  # 3 coordinates per evaluation point, 2 objects each with 3N points and 6 lines of conditions
-    for i in range(2):
-        # The centermost matrices: S11 S22 S33
-        S[0:M, i*N:(i+1)*N] = (r[i] ** 2 + 2 * eps ** 2 + dx[i] ** 2) / r_eps_cube[i] * oseen_factor[i]
-        S[M:2*M, (i+1)*N:(i+2)*N] = (r[i] ** 2 + 2 * eps ** 2 + dy[i] ** 2) / r_eps_cube[i] * oseen_factor[i]
-        S[2*M:3*M, (i+2)*N:(i+3)*N] = (r[i] ** 2 + 2 * eps ** 2 + dz[i] ** 2) / r_eps_cube[i] * oseen_factor[i]
-        
-        # Right part: S12 S13 S23
-        S[0:M, (i+1)*N:(i+2)*N] = dx[i] * dy[i] / r_eps_cube[i] * oseen_factor[i]
-        S[0:M, (i+2)*N:(i+3)*N] = dx[i]* dz[i] / r_eps_cube[i] * oseen_factor[i]
-        S[M:2*M, (i+2)*N:(i+3)*N] = dz[i] * dy[i] / r_eps_cube[i] * oseen_factor[i]
-        
-        # Left part: S21 S31, S32
-        S[M:2*M, 0:N] = dx[i] * dy[i] / r_eps_cube[i] * oseen_factor[i]
-        S[2*M:3*M, 0:N] = dx[i] * dz[i] / r_eps_cube[i] * oseen_factor[i]
-        S[2*M:3*M, N:2*N] = dz[i] * dy[i] / r_eps_cube[i] * oseen_factor[i]
-            
-    # -- Conditions -- 
-    # Force
-    force_matrix = np.zeros((3, 6*N))  # NOTE check om er rigtig!!!
-    for i in range(3):
-        force_matrix[i-3, i*N: (i+1)*N] = 1
-    S[-12: -9, :6*N] = force_matrix  # Row 
-    S[: 6*N, -12: -9] = force_matrix.T  # Column
-    S[-6: -3, : 6*N] = force_matrix
-    S[: 6*N, -6: -3] = force_matrix.T
+
+def oseen_tensor_two_objects(x_1, x_2, x_eval, x1_center, x2_center, dA, regularization_offset, viscosity):
+    """Two objects, calculate Oseen tensor in points outside of surface.
+
+    Args:
+        x_1 ((N, 3)-array): Coordinates for each point on the first object's surface centered around (0,0,0)
+        x_1 ((N, 3)-array): Coordinates for each point on the second object's surface centered around (0,0,0)
+        center_vector (3, 1)-array: Vector that points from object 1's center to object 2's center
+        epsilon (float): The regularization offset
+        dA (float): Area of each patch, assumed the same for all points
+        viscosity (float): Viscosity of the fluid.
+
+    Returns:
+        (6N1+12, 6N2+12)-array: Oseen tensor S, N1=len(x_1), N2=len(x_2)
+    """
+    N1 = len(x_1)
+    N2 = len(x_2)
+    N_eval = len(x_eval)
     
-    # Torque
-    torque_1 = torque_condition((dx1, dy1, dz1))
-    torque_2 = torque_condition((dx2, dy2, dz2))
-    S[-9: -6, : 3*N] = torque_1  # Row
-    S[:3*N, -9: -6] = torque_1.T  # Column
-    S[-3:, 3*N: 6*N] = torque_2
-    S[3*N: 6*N, -3:] = torque_2.T
-        
-    return S 
+    S = np.zeros((3*N_eval, 3*(N1+N2)))
+    S[:, : 3*N1] = bem.oseen_tensor(regularization_offset, dA, viscosity, evaluation_points=x_eval, source_points=x_1+x1_center)[:-6, :-6]  # Remove Force=0=Torque rows and columns
+    S[:, 3*N1 : 3*(N1+N2)] = bem.oseen_tensor(regularization_offset, dA, viscosity, evaluation_points=x_eval, source_points=x_2+x2_center)[:-6, :-6] 
+    return S
+    
 
-
-def force_on_sphere_two_objects(N1, max_mode, squirmer_radius, radius_obj2, center_vector, B, B_tilde, C, C_tilde, regularization_offset, viscosity, lab_frame=True):
+def force_surface_two_objects(N1, max_mode, squirmer_radius, radius_obj2, x1_center, x2_center, B, B_tilde, C, C_tilde, regularization_offset, viscosity, lab_frame=True, return_points=False):
     """Calculates the force vectors at N_sphere points on a sphere with radius squirmer_radius. 
 
     Args:
@@ -199,89 +168,121 @@ def force_on_sphere_two_objects(N1, max_mode, squirmer_radius, radius_obj2, cent
         (6N_sphere+12, 1)-array): Forces on the sphere. First N values are the x part, the next N the y and the last N the z part of the forces, then same for object 2 and +12 comes from conditions
     """
     # Get coordinates to points on the two surfaces
-    x_sphere_1, y_sphere_1, z_sphere_1, dA = bem.canonical_fibonacci_lattice(N1, squirmer_radius)
-    theta_sphere = np.arccos(z_sphere_1 / squirmer_radius) 
-    phi_sphere = np.arctan2(y_sphere_1, x_sphere_1)
-    x_surface_1 = np.stack((x_sphere_1, y_sphere_1, z_sphere_1)).T  
+    x1, y1, z1, dA = bem.canonical_fibonacci_lattice(N1, squirmer_radius)
+    theta = np.arccos(z1 / squirmer_radius) 
+    phi = np.arctan2(y1, x1)
+    x1_stacked = np.stack((x1, y1, z1)).T  
     
-    N2 = 4 * np.pi * radius_obj2 ** 2 // dA  # Ensures object 1 and 2 has same dA
-    x_sphere_2, y_sphere_2, z_sphere_2, _ = bem.canonical_fibonacci_lattice(N2, radius_obj2 / 2)
-    x_surface_2 = np.stack((x_sphere_2, y_sphere_2, z_sphere_2)).T  
+    N2 = int(4 * np.pi * radius_obj2 ** 2 / dA)  # Ensures object 1 and 2 has same dA
+    x2, y2, z2, _ = bem.canonical_fibonacci_lattice(N2, radius_obj2)
+    x2_stacked = np.stack((x2, y2, z2)).T  
 
-    A_oseen = oseen_tensor_surface_two_objects(x_surface_1, x_surface_2, center_vector,
+    A_oseen = oseen_tensor_surface_two_objects(x1_stacked, x2_stacked, x1_center, x2_center,
                                                dA, regularization_offset, viscosity)
     # Get velocities in each of the points for both objects
     ux1, uy1, uz1 = fv.field_cartesian(N=max_mode, r=squirmer_radius, 
-                                       theta=theta_sphere, phi=phi_sphere, 
+                                       theta=theta, phi=phi, 
                                        a=squirmer_radius, 
                                        B=B, B_tilde=B_tilde, 
                                        C=C, C_tilde=C_tilde, 
                                        lab_frame=lab_frame)
-
-    u_comb = np.array([ux1, uy1, uz1, np.zeros(12 + 3*N2)]).ravel()  # 2*6 zeros from Forces=0=Torqus + 3N2 zeros as Object 2 no own velocity
+    u_comb = np.array([ux1, uy1, uz1]).ravel()  
+    u_comb = np.append(u_comb, np.zeros(12 + 3*N2))  # 2*6 zeros from Forces=0=Torqus + 3N2 zeros as Object 2 no own velocity
+    
     # Solve for the forces, A_oseen @ forces = u_comb
     force_arr = np.linalg.solve(A_oseen, u_comb)
+    
+    if return_points:
+        return force_arr, x1_stacked, x2_stacked
     return force_arr 
 
 
-def test_oseen_two_objects_surface():
+# -- Test functions --
+def test_2obj_surface():
     eps = 0.1
     viscosity = 1
     N1 = 50
     max_mode = 2
     squirmer_radius = 1
     radius_obj2 = 0.5
-    center_vector = np.array([2 * squirmer_radius, 2 * squirmer_radius, 2 * squirmer_radius])
+    x1_center = np.zeros(3)
+    x2_center = np.array([2 * squirmer_radius, 2 * squirmer_radius, 2 * squirmer_radius])
+    B = np.zeros((max_mode+1, max_mode+1))
+    B_tilde = np.zeros_like(B)
+    C = np.zeros_like(B)
+    C_tilde = np.zeros_like(B)
+    B[1, 1] = 0
+        
+    force_surface_two_objects(N1, max_mode, squirmer_radius, radius_obj2, x1_center, x2_center, B, B_tilde, C, C_tilde, eps, viscosity, lab_frame=True)    
+
+
+def test_2obj_point():
+    import matplotlib.pyplot as plt
+    # Choose parameters
+    eps = 0.1
+    viscosity = 1
+    N1 = 50
+    max_mode = 3
+    squirmer_radius = 1
+    radius_obj2 = 0.7
+    x1_center = np.array([0, 2, 0])  # NOTE feltet afhænger af hvor man sætter squirmer!
+    x2_center = np.array([0, 0, 0])
     B = np.zeros((max_mode+1, max_mode+1))
     B_tilde = np.zeros_like(B)
     C = np.zeros_like(B)
     C_tilde = np.zeros_like(B)
     B[1, 1] = 1
-        
-    force_on_sphere_two_objects(N1, max_mode, squirmer_radius, radius_obj2, center_vector, B, B_tilde, C, C_tilde, eps, viscosity, lab_frame=True)    
-
-
-def test_oseen_given_force_two_objects():
-    eps = 0.1
-    viscosity = 1
-    xx = np.linspace(-2, 2, 25)
-    X, Y = np.meshgrid(xx, xx)
-    X = X.ravel()
-    Y = Y.ravel()
-    Z = 0 * X
     
-    # Source point and force
-    xi1 = np.zeros(3).reshape(-1, 1)
-    xi2 = np.zeros_like(xi1)
-    xi2[0] += 1
-    # F is Fx, Fy, Fz and Ux, Uy, Uz, and wx, wy, wz for each object, so 6N+12 = 30 long
-    F = np.zeros(30)
-    F[0] = 1.  # Fx1
-    F[3] = -1.  # Fx2
+    # Force
+    force_with_condition, x1_surface, x2_surface = force_surface_two_objects(N1, max_mode, squirmer_radius, radius_obj2, x1_center, x2_center, B, B_tilde, C, C_tilde, eps, viscosity, lab_frame=True, return_points=True)
+    translation = force_with_condition[-12: -6]
+    rotation = force_with_condition[-6:]
+    force = force_with_condition[:-12]  # Remove translation and rotation
     
-    x_e = np.stack((X, Y, Z)).T
-    O = oseen_tensor_point_two_objects(xi1, xi2, x_e, dA_1=0.5, dA_2=0.25, regularization_offset=eps, viscosity=viscosity)
-    v = O @ F
-    print(v.shape)
-    v = v[:-12]  # Remove force and torque  NOTE har fjernet 3 mindre...
-    print("v fjernet 12", v.shape)
-    v = np.reshape(v, (len(v)//6, 6), order='F')  # Fordel de seks hastigheder
-    print(v.shape) 
-    vx1 = v[:, 0]
-    vy1 = v[:, 1]
-    print(vx1.shape)
-    vx2 = v[:, 3]
-    vy2 = v[:, 4]
-    print(X.shape)
+    # Evaluation points
+    evaluation_points = np.linspace(-4, 4, 30)
+    X, Y = np.meshgrid(evaluation_points, evaluation_points)
+    x_e = X.ravel()
+    y_e = Y.ravel()
+    z_e = 0 * X.ravel()
+    x_e_stack = np.stack((x_e, y_e, z_e)).T
+    
+    # Oseen tensor in evaluation points
+    dA = 4 * np.pi * squirmer_radius ** 2 / N1
+    A_e = oseen_tensor_two_objects(x1_surface, x2_surface, x_e_stack, x1_center, x2_center, dA, eps, viscosity)
+    
+    # Velocity
+    v_e = A_e @ force
+    v_e = np.reshape(v_e, (len(v_e)//3, 3), order="F")
+    
+    # Remove values inside squirmer
+    r2_obj1 = np.sum((x_e_stack-x1_center)**2, axis=1)
+    r2_obj2 = np.sum((x_e_stack-x2_center)**2, axis=1)
+    v_e[r2_obj1 < squirmer_radius ** 2, :] = 0
+    v_e[r2_obj2 < radius_obj2 ** 2, :] = 0
+    
+    # -- Plot --
     fig, ax = plt.subplots(dpi=150, figsize=(6, 6))
-    ax.quiver(X, Y, vx1, vy1, color="red")
-    ax.quiver(X, Y, vx2, vy2, color="blue")
-    ax.set(xlabel="x", ylabel="y", title="With Conditions, Artificial 'force' $F_x$, lab frame")
+    # Add circles
+    circle_obj1 = plt.Circle(x1_center[:2], squirmer_radius, color="b", alpha=0.5)  # No need for z component
+    circle_obj2 = plt.Circle(x2_center[:2], radius_obj2, color="g", alpha=0.5)
+    ax.add_patch(circle_obj1)
+    ax.add_patch(circle_obj2)
+    # Add arrows
+    ax.quiver(x_e_stack[:, 0], x_e_stack[:, 1], v_e[:, 0], v_e[:, 1], color="red")
+    ax.set(xlabel="x", ylabel="y", title="Squirmer field two objects")
+    # Write translation and rotation on image
+    text_min = np.min(x_e)
+    text_max = np.max(x_e)
+    ax.text(text_min, text_max, s=f"U={np.round(translation, 4)}", fontsize=10)
+    ax.text(text_min, text_max-0.3, s=f"$\omega$={np.round(rotation, 4)}", fontsize=10)
+    ax.legend(["Squirmer", "Target"], loc="lower right")
+    fig.tight_layout()
+    plt.savefig("fluid/images/nytnavn.png")
     plt.show()
 
 
 if __name__ == "__main__":
-    import matplotlib.pyplot as plt
-    test_oseen_two_objects_surface()
+    test_2obj_point()
     
     
