93bb3ee9d41466acb667b6e68e997d18f828e695
2BeAsh <thefatfishmclol@gmail.com>
2023-05-03 17:04:56 +0200

    Update power_consumption.py
    Added n_sphere function

--- Fluid/power_consumption.py
+++ Fluid/power_consumption.py
@@ -1,6 +1,31 @@
 import numpy as np
 from scipy.special import factorial
 
+
+def n_sphere_angular_to_cartesian(phi, radius):
+    """ Convert n-sphere angles to cartesian coordinates. 
+    Formula: https://i.stack.imgur.com/CFSsT.png 
+    Taken from: https://stackoverflow.com/questions/20133318/n-sphere-coordinate-system-to-cartesian-coordinate-system
+
+    Args:
+        phi (ndarray): Angular coordinates, phi_1, phi_2, ..., phi_n-1
+        radius (float): Radius of the sphere
+    """
+    # Add extra term to phi
+    phi_expanded = np.concatenate((np.array([2*np.pi]), phi))  # using 2pi saves one operation in the cosine part
+    
+    # Sine factors with cumprod. First term is set to 1
+    sine = np.sin(phi_expanded)
+    sine[0] = 1
+    sine_cumprod = np.cumprod(sine)
+    
+    # Cosine
+    cosine = np.cos(phi_expanded)
+    cosine_roll = np.roll(cosine, -1)
+    
+    return radius * sine_cumprod * cosine_roll
+    
+
 def constant_power_factor(squirmer_radius, viscosity):
     """The factor in front of the mode terms which ensures that the power is constant
 
