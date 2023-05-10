1a8316661c8bc46b8c340d627ebfd5e29141116c
2BeAsh <thefatfishmclol@gmail.com>
2023-05-10 14:40:40 +0200

    Update pp_predict_direction.py

--- Reinforcement Learning/pp_predict_direction.py
+++ Reinforcement Learning/pp_predict_direction.py
@@ -2,9 +2,12 @@
 import numpy as np
 import os
 import sys
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 import matplotlib.lines as mlines
+import matplotlib.colors as colors
+from matplotlib.lines import Line2D
 
 import gym
 from gym import spaces
@@ -142,7 +145,7 @@ def train(N_surface_points, squirmer_radius, target_radius, max_mode, train_tota
 def plot_mode_vs_time(N_surface_points, N_iter, squirmer_radius, target_radius, max_mode):
     """Plot the actions taken at different iterations. Actions correspond to the weight/importance a mode is given.
     Color goes from bright to dark with increasing n and m values."""
-    
+        
     B_names = [r"$B_{01}$", r"$B_{02}$", r"$B_{03}$", r"$B_{04}$", r"$B_{11}$", r"$B_{12}$", r"$B_{13}$", 
                r"$B_{14}$", r"$B_{22}$", r"$B_{23}$", r"$B_{24}$", r"$B_{33}$", r"$B_{34}$", r"$B_{44}$",]
     B_tilde_names = [r"$B_{tilde,11}$", r"$B_{tilde,12}$", r"$B_{tilde,13}$", r"$B_{tilde,14}$", r"$B_{tilde,22}$", 
@@ -152,57 +155,143 @@ def plot_mode_vs_time(N_surface_points, N_iter, squirmer_radius, target_radius, 
     C_tilde_names = [r"$C_{tilde,12}$", r"$C_{tilde,13}$", r"$C_{tilde,14}$", r"$C_{tilde,22}$", 
                      r"$C_{tilde,23}$", r"$C_{tilde,24}$", r"$C_{tilde,33}$", r"$C_{tilde,34}$", r"$C_{tilde,44}$",]
     
+    B_len = len(B_names)
+    B_tilde_len = len(B_tilde_names)
+    C_len = len(C_names)
+    C_tilde_len = len(C_tilde_names)
+    
+    B_actions = np.empty((N_iter, B_len))
+    B_tilde_actions = np.empty((N_iter, B_tilde_len))
+    C_actions = np.empty((N_iter, C_len))
+    C_tilde_actions = np.empty((N_iter, C_tilde_len))
     
     # Load model and create environment
     model = PPO.load("ppo_predator_prey_direction")
     env = PredatorPreyEnv(N_surface_points, squirmer_radius, target_radius, max_mode)
-
-    # Run and render model
+    
+    # Run model N_iter times
     obs = env.reset()
-
-    B_actions = np.empty((N_iter, len(B_names)))
-    B_tilde_actions = np.empty((N_iter, len(B_tilde_names)))
-    C_actions = np.empty((N_iter, len(C_names)))
-    C_tilde_actions = np.empty((N_iter, len(C_tilde_names)))
-    
-    
     for i in range(N_iter):
         action, _states = model.predict(obs)
         obs, reward, done, info = env.step(action)
-        B_actions[i, :] = action[: len(B_names)]
-        B_tilde_actions[i, :] = action[len(B_names): len(B_names)+len(B_tilde_names)]
-        C_actions[i, :] = action[len(B_names)+len(B_tilde_names) : len(B_names)+len(B_tilde_names)+len(C_names)]
-        C_tilde_actions[i, :] = action[-len(C_tilde_actions): -1]
+        B_actions[i, :] = action[: B_len]
+        B_tilde_actions[i, :] = action[B_len: B_len+B_tilde_len]
+        C_actions[i, :] = action[B_len+B_tilde_len : B_len+B_tilde_len+C_len]
+        C_tilde_actions[i, :] = action[-C_tilde_len:]
     
-    # Plot mode values over iterations
-    fig, ax = plt.subplots(dpi=200, figsize=(8, 12))
-    ax.set(xlabel="Time", ylabel="Mode weight", title="Mode weights against time", xlim=(-0.5, N_iter-0.5))
+    
+    # Loop through each set of N_iter point such that it can get unique color
+    cm_norm = colors.Normalize(0, 1)    
+    def plot_N_iter_points(mode_values, cmap, marker):
+        length = mode_values.shape[1]
+        intensity = np.linspace(0.2, 1, length)
+        map = cm.ScalarMappable(norm=cm_norm, cmap=cmap)
+        lines = []
+        for i in range(length):
+            col = map.to_rgba(intensity[i])
+            line, = plt.plot(np.abs(mode_values[:, i]), marker, lw=1, ls="--", c=col)
+            lines.append(line)
+        return lines
 
-    B_line = ax.plot(np.arange(N_iter), B_actions, "--.", label=B_names)
-    B_tilde_line = ax.plot(np.arange(N_iter), B_tilde_actions, "--.", label=B_tilde_names)
+    # Figure setup    
+    fig, ax = plt.subplots(dpi=200, figsize=(8, 10))
+    ax.set(xlabel="Time", ylabel="Mode weight", title="Mode weights against time", xlim=(-0.5, N_iter-0.5), xticks=[])
+
+    B_lines = plot_N_iter_points(B_actions, "Reds", "x")
+    B_tilde_lines = plot_N_iter_points(B_tilde_actions, "Blues", ".")
+    # C_lines = plot_N_iter_points(C_actions, "Greens", "*")
+    # C_tilde_lines = plot_N_iter_points(C_tilde_actions, "Oranges", "<")
+
+    # Plot mode values over iterations
+    
+    
+    #B_line = ax.plot(B_actions, "--x", lw=1, )
+    #B_tilde_line = ax.plot(B_tilde_actions, "--.", lw=1)
 
     # Legend
-    lines = ax.get_lines()
-    B_legend = plt.legend(lines[i] for i in np.arange())
+    B_legend = plt.legend([B_lines[i] for i in np.arange(len(B_lines))], B_names, loc="upper left")
+    B_tilde_legend = plt.legend([B_tilde_lines[i] for i in np.arange(len(B_tilde_lines))], B_tilde_names, loc="upper right")
+
+    ax.add_artist(B_legend)
+    ax.add_artist(B_tilde_legend)
+    
     # https://stackoverflow.com/questions/12761806/matplotlib-2-different-legends-on-same-graph
+    # https://stackoverflow.com/questions/44911452/attributeerror-unknown-property-cmap 
     
-    
-    legend = [r"$B_{01}$", r"$B_{02}$", r"$B_{03}$", r"$B_{04}$", r"$B_{11}$", r"$B_{12}$", r"$B_{13}$", 
-              r"$B_{14}$", r"$B_{22}$", r"$B_{23}$", r"$B_{24}$", r"$B_{33}$", r"$B_{34}$", r"$B_{44}$",
-                
-              r"$B_{tilde,11}$", r"$B_{tilde,12}$", r"$B_{tilde,13}$", r"$B_{tilde,14}$", r"$B_{tilde,22}$", 
-              r"$B_{tilde,23}$", r"$B_{tilde,24}$", r"$B_{tilde,33}$", r"$B_{tilde,34}$", r"$B_{tilde,44}$",
-              
-              r"$C_{02}$", r"$C_{03}$", r"$C_{04}$", r"$C_{12}$", r"$C_{13}$", r"$C_{14}$", 
-              r"$C_{22}$", r"$C_{23}$", r"$C_{24}$", r"$C_{33}$", r"$C_{34}$", r"$C_{44}$",
-              
-              r"$C_{tilde,12}$", r"$C_{tilde,13}$", r"$C_{tilde,14}$", r"$C_{tilde,22}$", 
-              r"$C_{tilde,23}$", r"$C_{tilde,24}$", r"$C_{tilde,33}$", r"$C_{tilde,34}$", r"$C_{tilde,44}$",
-              ]
+
     
     fig.tight_layout()
     plt.show()
     
+    
+def plot_mode_choice(N_surface_points, N_iter, squirmer_radius, target_radius, max_mode):
+    """Plot the actions taken at different iterations. Actions correspond to the weight/importance a mode is given.
+    Color goes from bright to dark with increasing n and m values."""
+    # Add more colors
+    mpl.rcParams["axes.prop_cycle"] = mpl.cycler(color=['blue', 'green', 'red', 'cyan', 'magenta', 'yellow', 'black', 
+                                                        'purple', 'pink', 'brown', 'orange', 'teal', 'coral', 'lightblue', 
+                                                        'lime', 'lavender', 'turquoise', 'darkgreen', 'tan', 'salmon', 'gold'])
+
+    B_names = [r"$B_{01}$", r"$B_{02}$", r"$B_{03}$", r"$B_{04}$", r"$B_{11}$", r"$B_{12}$", r"$B_{13}$", 
+               r"$B_{14}$", r"$B_{22}$", r"$B_{23}$", r"$B_{24}$", r"$B_{33}$", r"$B_{34}$", r"$B_{44}$",]
+    B_tilde_names = [r"$\tilde{B}_{11}$", r"$\tilde{B}_{12}$", r"$\tilde{B}_{13}$", r"$\tilde{B}_{14}$", r"$\tilde{B}_{22}$", 
+                     r"$\tilde{B}_{23}$", r"$\tilde{B}_{24}$", r"$\tilde{B}_{33}$", r"$\tilde{B}_{34}$", r"$\tilde{B}_{44}$",]
+    C_names = [r"$C_{02}$", r"$C_{03}$", r"$C_{04}$", r"$C_{12}$", r"$C_{13}$", r"$C_{14}$", 
+               r"$C_{22}$", r"$C_{23}$", r"$C_{24}$", r"$C_{33}$", r"$C_{34}$", r"$C_{44}$",]
+    C_tilde_names = [r"$\tilde{C}_{12}$", r"$\tilde{C}_{13}$", r"$\tilde{C}_{14}$", r"$\tilde{C}_{22}$", 
+                     r"$\tilde{C}_{23}$", r"$\tilde{C}_{24}$", r"$\tilde{C}_{33}$", r"$\tilde{C}_{34}$", r"$\tilde{C}_{44}$",]
+    
+    B_len = len(B_names)
+    B_tilde_len = len(B_tilde_names)
+    C_len = len(C_names)
+    C_tilde_len = len(C_tilde_names)
+    
+    B_actions = np.empty((N_iter, B_len))
+    B_tilde_actions = np.empty((N_iter, B_tilde_len))
+    C_actions = np.empty((N_iter, C_len))
+    C_tilde_actions = np.empty((N_iter, C_tilde_len))
+    
+    # Load model and create environment
+    model = PPO.load("ppo_predator_prey_direction")
+    env = PredatorPreyEnv(N_surface_points, squirmer_radius, target_radius, max_mode)
+    
+    # Run model N_iter times
+    obs = env.reset()
+    for i in range(N_iter):
+        action, _states = model.predict(obs)
+        obs, reward, done, info = env.step(action)
+        B_actions[i, :] = action[: B_len]
+        B_tilde_actions[i, :] = action[B_len: B_len+B_tilde_len]
+        C_actions[i, :] = action[B_len+B_tilde_len : B_len+B_tilde_len+C_len]
+        C_tilde_actions[i, :] = action[-C_tilde_len:]
+
+    # Plot
+    fig, ax = plt.subplots(nrows=2, ncols=2, dpi=200)
+    ax1 = ax[0, 0]
+    ax2 = ax[0, 1]
+    ax3 = ax[1, 0]
+    ax4 = ax[1, 1]
+
+    def fill_axis(axis, y, marker, label, title):        
+        axis.set(xticks=[])
+        axis.set_title(title, fontsize=7)
+        axis.plot(np.abs(y), marker=marker, ls="--", lw=0.75)
+        axis.legend(label, fontsize=4, bbox_to_anchor=(1.05, 1), 
+                    loc='upper left', borderaxespad=0.)
+
+        
+    seperate_letters = True
+    if seperate_letters:
+        fill_axis(ax1, B_actions, ".", B_names, title=r"$B$ modes")
+        fill_axis(ax2, B_tilde_actions, ".", B_tilde_names, title=r"$\tilde{B}$ modes")
+        fill_axis(ax3, C_actions, ".", C_names, title=r"$C$ modes")
+        fill_axis(ax4, C_tilde_actions, ".", C_tilde_names, title=r"$\tilde{C}$ modes")
+
+    ax2.set(yticks=[])
+    ax4.set(yticks=[])
+    
+    fig.tight_layout()
+    plt.show()
 
 # -- Run the code --
 # Parameters
@@ -210,13 +299,14 @@ N_surface_points = 80
 squirmer_radius = 1
 target_radius = 1.1
 max_mode = 4
-N_iter = 2
+N_iter = 5
 
 train_total_steps = int(2e5)
 
 #check_model(N_surface_points, squirmer_radius, target_radius, max_mode)
 #train(N_surface_points, squirmer_radius, target_radius, max_mode, train_total_steps)
-plot_mode_vs_time(N_surface_points, N_iter, squirmer_radius, target_radius, max_mode)
+#plot_mode_vs_time(N_surface_points, N_iter, squirmer_radius, target_radius, max_mode)
+plot_mode_choice(N_surface_points, N_iter, squirmer_radius, target_radius, max_mode)
 
 
 # If wants to see reward over time, write the following in cmd in the log directory
