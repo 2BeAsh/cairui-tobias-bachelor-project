ded788ac5cff357b4fb2f67af18d97e6aed03fd8
CairuiDuan <cairduan@gmail.com>
2023-05-25 17:02:08 +0200

    train and plot

--- RL/pp_predict_direction.py
+++ RL/pp_predict_direction.py
@@ -452,13 +452,13 @@ tot_radius = squirmer_radius + target_radius
 target_initial_position = [1.3*tot_radius, 1.3*tot_radius] / np.sqrt(2)
 max_mode = 2
 viscosity = 1
-sensor_noise = 0.20
-train_total_steps = int(5.5e5)
+sensor_noise = 0.24
+train_total_steps = int(8.5e5)
 
 # Plotting parameters
 N_iter = 11
-PPO_number = 5 # For which model to load when plotting, after training
-PPO_list = [2,3, 4, 5]
+PPO_number = 6 # For which model to load when plotting, after training
+PPO_list = [2,3, 4, 5, 6, 7]
 
 #check_model(N_surface_points, squirmer_radius, target_radius, max_mode, sensor_noise, target_initial_position)
 train(N_surface_points, squirmer_radius, target_radius, max_mode, sensor_noise, target_initial_position, viscosity, train_total_steps)
