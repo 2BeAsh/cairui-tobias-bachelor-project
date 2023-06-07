c46d95523a982a96f23ffe0313e37d25a2c42923
CairuiDuan <cairduan@gmail.com>
2023-06-07 15:50:27 +0200

    train

--- RL/RL_functions.py
+++ RL/RL_functions.py
@@ -43,12 +43,12 @@ max_mode_direction = 2
 sensor_noise = 0.18
 reg_offset = 0.05
 coord_plane = "xy"
-train_total_steps_direction = int(6e5)
+train_total_steps_direction = int(8e5)
 
 # Variables
 target_radius_variable = 0.5
 target_initial_position_variable = [1.7, 1.7] / np.sqrt(2)
-angle_variable = 0
+angle_variable = np.pi/4
 sensor_noise_variable = 0.08
 
 # - Predict direction functions -
