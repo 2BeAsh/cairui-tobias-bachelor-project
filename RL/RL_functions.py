70a45e412a79ab874d46b81cdde49b4e9282d1f2
CairuiDuan <cairduan@gmail.com>
2023-06-10 07:38:50 +0200

    train

--- RL/RL_functions.py
+++ RL/RL_functions.py
@@ -48,7 +48,7 @@ train_total_steps_direction = int(3.5e5)
 # Variables
 target_radius_variable = 0.6
 target_initial_position_variable = [2.2, 2.2] / np.sqrt(2)
-angle_variable = -3*np.pi/4
+angle_variable = 3*np.pi/4
 sensor_noise_variable = 0.14
 
 # - Predict direction functions -
